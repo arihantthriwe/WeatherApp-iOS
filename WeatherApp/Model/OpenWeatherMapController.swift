66e64c000e567e8ec6da9d4d6e9e29b70bca4a6b
Arihant Thriwe <arihant.jain@thriwe.com>
2022-04-07 01:51:59 +0530

    Access Keys from plist

--- WeatherApp/Model/OpenWeatherMapController.swift
+++ WeatherApp/Model/OpenWeatherMapController.swift
@@ -7,12 +7,33 @@
 
 import Foundation
 
+class APIKEY{
+    var config: [String: Any]?
+    public func getKey() -> String?{
+        if let infoPlistPath = Bundle.main.url(forResource: "Keys", withExtension: "plist") {
+            do {
+                let infoPlistData = try Data(contentsOf: infoPlistPath)
+                
+                if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: Any] {
+                    config = dict
+                }
+            } catch {
+                return nil
+            }
+        }
+        print(config?["APIKey"] as! String)
+        return config?["APIKey"] as! String?
+    }
+}
+
 class OpenWeatherMapController: WebServiceController{
     // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     // https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
-    
+    lazy var APIKey: String? = {
+        return APIKEY().getKey()
+    }()
+    let endPoint = "https://api.openweathermap.org/data/2.5/weather?q=\("city")&appid="
     func fetchWeatherDta(for city: String, completionHandler: (String?, WebServiceControllerError?) -> Void) {
-        <#code#>
     }
     
     
